e0929c46a7adf94ff990f235130a69b8fbf4bda0
dest92 <acebalmatias2019@gmail.com>
2023-01-18 20:43:47 -0300

    Structure

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello Twitty!")
+}
+
+//mongodb+srv://Twitty:<password>@cluster0.qycp3ec.mongodb.net/?retryWrites=true&w=majority
