70acff16652ef7f3ecc7c1f76c5f56ffbb2dd528
dest92 <acebalmatias2019@gmail.com>
2023-02-20 20:55:41 -0300

    React bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.scss';
+import React from "react";
+import { useState } from "react";
+import { Button } from "react-bootstrap";
 
-function App() {
+export default function App() {
+  const [user, setUser] = useState();
+
+  // return (
+  //   <div>{user ? <h1>You´re logged</h1> : <h1>You´re not logged</h1>}</div>
+  // );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button>Login</Button>
     </div>
   );
 }
-
-export default App;
